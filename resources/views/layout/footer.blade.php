5508805ac5402b9df668b6972a8e0419c6a21112
Yaqin1 <ahmadnuryaqin05@gmail.com>
2022-09-26 12:16:01 +0700

    java script

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 1.0.0
+            </div>
+            <strong>Copyright &copy; 2022-2023 <a href="https://adminlte.io">SMK Antartika 1 Sidoarjo</a>.</strong> All rights
+            reserved.
+        </footer>
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,66 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <a href="../../index3.html" class="brand-link">
+        <span class="brand-text font-weight-light">Smk Antartika 1 Sidoarjo</span>
+    </a>
+
+    <div class="sidebar">
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a href="#" class="d-block">Ahmad Nur Yaqin</a>
+            </div>
+        </div>
+
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+                <li class="nav-header">DASHBOARD</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="fas fa-compass"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+
+
+                <li class="nav-header">MASTER</li>
+                <li class="nav-item">
+                    <a href="{{ route('guru.index') }}" class="nav-link">
+                        <i class="fa fa-user-tie"></i>
+                        <p>
+                             Guru
+                        </p>
+                    </a>
+
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                        <p>
+                             Kelas
+                        </p>
+                    </a>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-bible"></i>
+                        <p>
+                            Mapel
+                        </p>
+                    </a>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-user-graduate"></i>
+                        <p>
+                             Siswa
+                        </p>
+                    </a>
+
+        </nav>
+
+    </div>
+
+</aside>
